f708fcb798d72755d859eb09d534da033fece2fb
DeepxHyeon <deepxhyeon@gmail.com>
2020-08-07 17:56:37 +0900

    #8.5 Photo Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -10,9 +11,8 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ("name", "used_by")
 
     def used_by(self, obj):
-        return obj.rooms.count()
-
-    pass
+        print(obj.file)
+        return ""
 
 
 @admin.register(models.Room)
@@ -86,6 +86,11 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
 
-    """ """
+    """ Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width="50px" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
