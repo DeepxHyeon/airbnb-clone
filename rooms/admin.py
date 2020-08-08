a73dd08ae45ccb04bdbd7c3a80b3821b7a606ad2
DeepxHyeon <deepxhyeon@gmail.com>
2020-08-08 17:12:50 +0900

    #9.3 seed_rooms part One

--- rooms/admin.py
+++ rooms/admin.py
@@ -31,7 +31,17 @@ class RoomAdmin(admin.ModelAdmin):
     fieldsets = (
         (
             "Basic Info",
-            {"fields": ("name", "description", "country", "city", "address", "price")},
+            {
+                "fields": (
+                    "name",
+                    "description",
+                    "country",
+                    "city",
+                    "address",
+                    "price",
+                    "room_type",
+                )
+            },
         ),
         ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
         ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
@@ -65,10 +75,10 @@ class RoomAdmin(admin.ModelAdmin):
         "instant_book",
         "host__superhost",
         "host__gender",
+        "room_type",
         "amenities",
         "facilities",
         "house_rules",
-        "room_type",
         "city",
         "country",
     )
