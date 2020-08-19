af73b1252f2ce773a44874423ca6435f7f44f5bb
DeepxHyeon <deepxhyeon@gmail.com>
2020-08-19 17:08:58 +0900

    #12.0 URLs and Arguments

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
+    path("rooms/", include("rooms.urls", namespace="rooms")),
     path("admin/", admin.site.urls),
+
 ]
 
 if settings.DEBUG:
